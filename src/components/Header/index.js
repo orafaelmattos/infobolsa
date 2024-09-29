d7e3b3d9e81e1585898597649b373990e667f050
Rafael Mattos <142951733+orafaelmattos@users.noreply.github.com>
2024-09-29 16:59:49 -0300

    Adicionando o valor do dollar no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import './header.css';
 import logo from '../../assets/logo.png';
+import bandeira from '../../assets/estados-unidos.png'
 import { FiMenu } from "react-icons/fi";
 import { ApiContext } from '../../contexts/contextApi';
 
@@ -22,7 +23,15 @@ export default function Header(){
                 <h2>Criptomoedas</h2>
             </div>
 
-            <h1><FiMenu /></h1>
+            <h1><FiMenu/></h1>
+
+            <div className='cotation-dollar'>
+                <span><h2>Dólar Americano:</h2></span>
+                <div className='cotation-coin'>
+                    <img src={bandeira} alt='Dollar' />
+                    <h2>$ {data?.currencies?.USD.buy.toFixed(2)}</h2>
+                </div>
+            </div>
 
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import ContextApi from '../src/contexts/contextApi.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ContextApi>
       <App />
     </ContextApi>
-  </React.StrictMode>
 );
 
