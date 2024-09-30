d1348c1eb5ba00b82245ca750fbc610ecb81aea7
Rafael Mattos <142951733+orafaelmattos@users.noreply.github.com>
2024-09-30 19:57:21 -0300

    Criando o banner da main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,22 @@
 import './main.css';
 import Header from '../../components/Header/index';
+import Image from '../../assets/bolsa de valores.jpg';
+import Logo from '../../assets/logo.png';
 
 export default function Main(){
     return(
         <div className='container'>
             <Header/>
-            <p>ola</p>
+            <section className='image-container'>
+                <img src={Image} alt='image-background' className='image-background'/>
+                <div className='content'>
+                    <img src={Logo} alt='logo'/>
+                    <div className='title'>
+                        <h1 className='cssanimation typing'>Seu lugar na bolsa</h1>
+                        <p>Explore as oportunidades do mercado financeiro.</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
