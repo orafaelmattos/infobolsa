ed003736964464d730c6058ae6bed6bbf44d5a59
Rafael Mattos <142951733+orafaelmattos@users.noreply.github.com>
2024-09-30 22:06:56 -0300

    Criando os cards

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,8 +2,15 @@ import './main.css';
 import Header from '../../components/Header/index';
 import Image from '../../assets/bolsa de valores.jpg';
 import Logo from '../../assets/logo.png';
+import { TbZoomMoney } from "react-icons/tb";
+import { PiMoney } from "react-icons/pi";
+import { useContext } from 'react';
+import { ApiContext } from '../../contexts/contextApi';
 
 export default function Main(){
+
+    const { data } = useContext(ApiContext);
+
     return(
         <div className='container'>
             <Header/>
@@ -17,6 +24,64 @@ export default function Main(){
                     </div>
                 </div>
             </section>
+            
+            <div className="card-container">
+                <h1>Indicadores Econômicos</h1>
+                <h2>Fique por dentro de todas as informações da economia mundial.</h2>
+
+                <div className='cards'>
+                    <div className='card-border'>
+                        <div className='card'>
+                            <div className='card-image'>
+                                <h1><TbZoomMoney/></h1>
+                            </div>
+
+                            <div className='card-info'>
+                                <p className='card-title'>{data?.stocks?.IBOVESPA.name}</p>
+                                <div className='card-variation'>
+                                    <p>{data?.stocks?.IBOVESPA.points.toFixed(2)}</p>
+                                    <p style={{ color: data?.stocks?.IBOVESPA.variation < 0 ? '#FF033E' : 'green', fontSize: '12px' }} >{data?.stocks?.IBOVESPA.variation.toFixed(2)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='card-border'>
+                        <div className='card'>
+                            <div className='card-image'>
+                                <h1><PiMoney /></h1>
+                            </div>
+
+                            <div className='card-info'>
+                                <p className='card-title'>{data?.currencies?.USD.name}</p>
+                                <div className='card-variation'>
+                                    <p>USD ${data?.currencies?.USD.buy}</p>
+                                    <p style={{ color: data?.currencies?.USD.variation < 0 ? '#FF033E' : 'green', fontSize: '12px' }} >{data?.currencies?.USD.variation.toFixed(2)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='card-border'>
+                        <div className='card'>
+                            <div className='card-image'>
+                                <h1><TbZoomMoney/></h1>
+                            </div>
+
+                            <div className='card-info'>
+                                <p className='card-title'>{data?.bitcoin?.mercadobitcoin.name}</p>
+                                <div className='card-variation'>
+                                    <p>{data?.bitcoin?.mercadobitcoin.buy.toFixed(2)}</p>
+                                    <p style={{ color: data?.bitcoin?.mercadobitcoin.variation < 0 ? '#FF033E' : 'green', fontSize: '12px' }} >{data?.bitcoin?.mercadobitcoin.variation.toFixed(2)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
         </div>
     )
 }
